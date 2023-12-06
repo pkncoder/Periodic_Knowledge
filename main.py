bad338d93afd2a609f239e9e6682b0e35d40058a
pkncoder <128310642+pkncoder@users.noreply.github.com>
2023-12-06 17:12:11 +0000

    Getting ready to use the csv file data

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from Table import *
 from sys import argv
+import csv
+#print(findElement(argv[1]))
 
-print(findElement(argv[1]))
+with open('data.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        print(f'{row[0]}')
+        print(f'{row[1]}')
